3bf1083a60b5079bbcb3ccf511804424021977df
= <tom@tomcheney.com>
2022-08-19 16:08:13 +0100

    log some more instance specific stuff

--- NLog.Targets.InfluxDB/InfluxDB.cs
+++ NLog.Targets.InfluxDB/InfluxDB.cs
@@ -3,6 +3,8 @@
 using InfluxDB.Client;
 using InfluxDB.Client.Api.Domain;
 using InfluxDB.Client.Writes;
+using System;
+using System.Net;
 
 namespace NLog.Targets.InfluxDB
 {
@@ -32,6 +34,10 @@ namespace NLog.Targets.InfluxDB
 
         InfluxDBClient m_influxClient;
         WriteApi m_writeApi;
+        string m_application = "";
+        string m_version = "";
+        string m_hostName = Dns.GetHostName();
+        string m_guid = Guid.NewGuid().ToString();
 
         protected override void Dispose(bool disposing)
         {
@@ -47,12 +53,20 @@ namespace NLog.Targets.InfluxDB
             m_influxClient = InfluxDBClientFactory.Create(Host, Token);
             m_writeApi = m_influxClient.GetWriteApi();
 
+            var name = System.Reflection.Assembly.GetEntryAssembly().GetName();
+
+            m_application = name.Name;
+            m_version = name.Version.ToString();
         }
         protected override void Write(LogEventInfo logEvent)
         {
             var point = PointData.Measurement("NLog")
                 .Tag("LoggerName", logEvent.LoggerName)
                 .Tag("Level", logEvent.Level.ToString())
+                .Tag("Name", m_application)
+                .Tag("Version", m_version)
+                .Tag("GUID", m_guid)
+                .Tag("Host", m_hostName)
                 .Field("Message", logEvent.Message)
                 .Timestamp(logEvent.TimeStamp.ToUniversalTime(), WritePrecision.Ns);
             m_writeApi.WritePoint(point, Bucket, Org);
