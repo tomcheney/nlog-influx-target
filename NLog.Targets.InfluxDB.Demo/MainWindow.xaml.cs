680cc64fe6cbaf6fac930de12894e966aab0b13b
= <tom@tomcheney.com>
2022-08-19 14:31:15 +0100

    initial commit

---
+++ NLog.Targets.InfluxDB.Demo/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NLog.Targets.InfluxDB.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Info("Some Info");
+        }
+
+        private void Debug_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Debug("Some Debug");
+        }
+
+        private void Warn_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Warn("Warning!!");
+        }
+
+        private void Error_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Error("Error!!");
+        }
+    }
+}
---
+++ NLog.Targets.InfluxDB/InfluxDB.cs
@@ -0,0 +1,61 @@
+﻿using NLog.Config;
+
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+
+namespace NLog.Targets.InfluxDB
+{
+    [Target("InfluxDB")]
+    public sealed class InfluxDB : Target
+    {
+        public InfluxDB()
+        {
+            //set defaults
+            this.Host = "";
+            this.Bucket = "";
+            this.Org = "";
+            this.Token = "";
+        }
+
+        [RequiredParameter]
+        public string Host { get; set; }
+
+        [RequiredParameter]
+        public string Bucket { get; set; }
+
+        [RequiredParameter]
+        public string Token { get; set; }
+
+        [RequiredParameter]
+        public string Org { get; set; }
+
+        InfluxDBClient m_influxClient;
+        WriteApi m_writeApi;
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            m_writeApi.Flush();
+            m_writeApi.Dispose();
+            m_influxClient.Dispose();
+        }
+
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+            m_influxClient = InfluxDBClientFactory.Create(Host, Token);
+            m_writeApi = m_influxClient.GetWriteApi();
+
+        }
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var point = PointData.Measurement("NLog")
+                .Tag("LoggerName", logEvent.LoggerName)
+                .Tag("Level", logEvent.Level.ToString())
+                .Field("Message", logEvent.Message)
+                .Timestamp(logEvent.TimeStamp.ToUniversalTime(), WritePrecision.Ns);
+            m_writeApi.WritePoint(point, Bucket, Org);
+        }
+    }
+}
