7a145979a0ef7b8005f11611f7a58d04766bf746
= <tom@tomcheney.com>
2022-08-20 12:20:48 +0100

    Add tag and field set parameters

--- NLog.Targets.InfluxDB/InfluxDB.cs
+++ NLog.Targets.InfluxDB/InfluxDB.cs
@@ -6,11 +6,12 @@ using InfluxDB.Client.Writes;
 using System;
 using System.Net;
 using NLog.Layouts;
+using System.Collections.Generic;
 
 namespace NLog.Targets.InfluxDB
 {
     [Target("InfluxDB")]
-    public sealed class InfluxDB : Target
+    public class InfluxDB : Target
     {
         public InfluxDB()
         {
@@ -33,12 +34,17 @@ namespace NLog.Targets.InfluxDB
         [RequiredParameter]
         public Layout Org { get; set; }
 
+        [DefaultParameter]
+        public Layout Measurement { get; set; } = "NLog";
+
+        [ArrayParameter(typeof(TargetPropertyWithContext), "tag")]
+        public virtual IList<TargetPropertyWithContext> Tags { get; } = new List<TargetPropertyWithContext>();
+
+        [ArrayParameter(typeof(TargetPropertyWithContext), "field")]
+        public virtual IList<TargetPropertyWithContext> Fields { get; } = new List<TargetPropertyWithContext>();
+
         InfluxDBClient m_influxClient;
         WriteApi m_writeApi;
-        string m_application = "";
-        string m_version = "";
-        string m_hostName = Dns.GetHostName();
-        string m_guid = Guid.NewGuid().ToString();
 
         protected override void Dispose(bool disposing)
         {
@@ -50,30 +56,60 @@ namespace NLog.Targets.InfluxDB
 
         protected override void InitializeTarget()
         {
-            base.InitializeTarget();
             var host = RenderLogEvent(Host, LogEventInfo.CreateNullEvent());
             var token = RenderLogEvent(Token, LogEventInfo.CreateNullEvent());
             m_influxClient = InfluxDBClientFactory.Create(host, token);
             m_writeApi = m_influxClient.GetWriteApi();
 
-            var name = System.Reflection.Assembly.GetEntryAssembly().GetName();
 
-            m_application = name.Name;
-            m_version = name.Version.ToString();
+            if (Tags.Count == 0)
+            {
+                var entryAssembly = System.Reflection.Assembly.GetEntryAssembly()?.GetName();
+                var applicationName = entryAssembly?.Name;
+                var applicationVersion = entryAssembly?.Version.ToString();
+                var instanceGuid = Guid.NewGuid().ToString();
+                var hostName = Dns.GetHostName();
+
+                Tags.Add(new TargetPropertyWithContext("LoggerName", "${logger}"));
+                Tags.Add(new TargetPropertyWithContext("Level", "${level}"));
+                Tags.Add(new TargetPropertyWithContext("Application", applicationName));
+                Tags.Add(new TargetPropertyWithContext("Version", applicationVersion));
+                Tags.Add(new TargetPropertyWithContext("GUID", instanceGuid));
+                Tags.Add(new TargetPropertyWithContext("Host", hostName));
+            }
+
+            if (Fields.Count == 0)
+            {
+                Fields.Add(new TargetPropertyWithContext("Message", "${message:raw=true}"));
+            }
+
+            base.InitializeTarget();
+
         }
         protected override void Write(LogEventInfo logEvent)
         {
-            var point = PointData.Measurement("NLog")
-                .Tag("LoggerName", logEvent.LoggerName)
-                .Tag("Level", logEvent.Level.ToString())
-                .Tag("Application", m_application)
-                .Tag("Version", m_version)
-                .Tag("GUID", m_guid)
-                .Tag("Host", m_hostName)
-                .Field("Message", logEvent.Message)
-                .Field("level", logEvent.Level.ToString().ToLower())
-                .Timestamp(logEvent.TimeStamp.ToUniversalTime(), WritePrecision.Ns);
-            
+            var measurement = RenderLogEvent(Measurement, logEvent);
+            var point = PointData.Measurement(measurement);
+            for (int i = 0; i < Tags.Count; ++i)
+            {
+                var tag = Tags[i];
+                var tagValue = RenderLogEvent(tag.Layout, logEvent);
+                if (string.IsNullOrEmpty(tagValue))
+                    continue;
+
+                point = point.Tag(tag.Name, tagValue);
+            }
+
+            for (int i = 0; i < Fields.Count; ++i)
+            {
+                var field = Fields[i];
+                var fieldValue = RenderLogEvent(field.Layout, logEvent);
+                if (string.IsNullOrEmpty(fieldValue))
+                    continue;
+
+                point = point.Field(field.Name, fieldValue);
+            }
+
             var bucket = RenderLogEvent(Bucket, logEvent);
             var organization = RenderLogEvent(Org, logEvent);
             m_writeApi.WritePoint(point, bucket, organization);
